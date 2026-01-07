afa5de51fde7b1cd419caf558d734b373c23686f
faruklmu17 <farukqmul@gmail.com>
2026-01-06 19:57:17 -0500

    added new code

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# I deleted the code!
+myDictionary = {1: "-", 2: "-", 3: "-"}
+def dictFunction(dashes):
+    print(dashes[1])
+dictFunction(myDictionary[1])
+
+#without adding any new line, fix the code!
