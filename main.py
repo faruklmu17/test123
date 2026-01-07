776bd50e8d056f0b54c1bfa54b8f8671927427d2
faruklmu17 <farukqmul@gmail.com>
2026-01-06 20:08:10 -0500

    changes made

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-myDictionary = {1: "-", 2: "-", 3: "-"}
+
+
+myDictionary = {1: "$#", 2: "*", 3: "!"}
+
+#print(myDictionary[2])
+
 def dictFunction(dashes):
+
     print(dashes[1])
+
 dictFunction(myDictionary[1])
 
 #without adding any new line, fix the code!
+
+#Watson 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Bella 
